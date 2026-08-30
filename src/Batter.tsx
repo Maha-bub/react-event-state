c08e9315c801c52c83e27fc9c86eecbb51ed580e
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-08-30 10:40:36 +0600

    explore useState and set counter

--- src/Batter.tsx
+++ src/Batter.tsx
@@ -6,10 +6,20 @@ export default function Batter() {
     const handleRuns = () => {
         setnewRuns(newRuns + 1);
     }
+    const hanldeAddtow = () => {
+        setnewRuns(newRuns + 2)
+    }
+    const handleAddthree = () => {
+        setnewRuns(newRuns + 3);
+    }
     return (
         <>
             <h3>RunsTotal:{newRuns}</h3>
             <button onClick={handleRuns}>Add Single Runs</button>
+            <button onClick={hanldeAddtow}>add two</button>
+            <button onClick={handleAddthree}>add three</button>
+            <button>add four</button>
+            <button>add six</button>
         </>
     )
 }
