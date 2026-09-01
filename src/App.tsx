87771715b14541d8d85ef99ff19a13ab9fd9c922
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-09-01 19:00:41 +0600

    create user component and Promise for user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
+import { Suspense } from 'react';
 import './App.css'
 import Batter from './Batter'
+import User from './User';
+
 
 function App() {
-  function handleButtonClick() {
-    alert('button clicked!')
+
+  const userDataPromise = async () =>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    return data;
   }
+  // function handleButtonClick() {
+  //   alert('button clicked!')
+  // }
 
   return (
     <>
-      <button onlclick='handleButtonClick()'>Click old style</button>
+
+    <Suspense fallback={<p>User Loading....</p>}>
+    <User userDataPromise={userDataPromise}></User>
+    </Suspense>
+      {/* <button onlclick='handleButtonClick()'>Click old style</button>
       <button onClick={handleButtonClick}>Click me</button>
       <p>---------------</p>
 
-      <Batter></Batter>
+      <Batter></Batter> */}
 
 
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/User.tsx
@@ -0,0 +1,10 @@
+const User = () => {
+   
+    return (
+        <div>
+            <h3>UserName:{user.name}</h3>
+        </div>
+    );
+};
+
+export default User;
